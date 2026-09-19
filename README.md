325dab5e395433ea3b4076a89527e29d4eef0984
kirk-en <kirkenbysk@gmail.com>
2026-09-18 20:53:06 -0400

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Warped playlist generator
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,11 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="home-page">
+      <p>Warped Playlist</p>
+    </div>
+  );
+}
+
+export default HomePage;
